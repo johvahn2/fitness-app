d883a2db25376b794ad39a61470e7e5fc189e4e2
Johvahn Brodie <johvahn.brodie@ab.gov.ag>
2019-06-03 12:30:36 -0400

    first commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+
+// Material
+import {
+    MatInputModule,
+    MatToolbarModule,
+	MatPaginatorModule,
+	MatSortModule,
+	MatTableModule,
+	MatSelectModule,
+	MatMenuModule,
+	MatProgressBarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatSidenavModule,
+	MatCheckboxModule,
+	MatTabsModule,
+	MatNativeDateModule,
+	MatCardModule,
+	MatRadioModule,
+	MatIconModule,
+	MatDatepickerModule,
+	MatAutocompleteModule,
+	MatSlideToggleModule,
+	MatSnackBarModule,
+	MatTooltipModule
+} from '@angular/material';
+
+const materials = [
+    MatInputModule,
+    MatToolbarModule,
+	MatPaginatorModule,
+	MatSortModule,
+	MatTableModule,
+	MatSelectModule,
+	MatMenuModule,
+	MatProgressBarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatSidenavModule,
+	MatCheckboxModule,
+	MatTabsModule,
+	MatNativeDateModule,
+	MatCardModule,
+	MatRadioModule,
+	MatIconModule,
+	MatDatepickerModule,
+	MatAutocompleteModule,
+	MatSlideToggleModule,
+	MatSnackBarModule,
+	MatTooltipModule
+  ];
+
+  @NgModule({
+    declarations: [
+    ],
+    imports: [
+      materials
+    ],
+    exports:[materials]
+  })
+  export class MaterialModule { }
